5adf1e6b1ca04eeca4675417eeeea9ff3563dfea
sahrt <sahricy5@gmail.com>
2023-11-09 02:34:43 +0700

    memperbaiki bug cache dan menambhakan erty point namun belum bisa diakses

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Tambahkan perintah-perintah Artisan atau tugas lain yang ingin Anda jalankan di sini
-if [ ! -e /var/www/app/initialized ]; then
+
     php artisan optimize
     php artisan cache:clear
     php artisan config:cache
     php artisan migrate:fresh
     php artisan db:seed
-    touch /var/www/app/initialized
-fi
+
 # Lanjutkan dengan menjalankan perintah yang akan memulai layanan web, misalnya menjalankan Apache atau Nginx
 exec "$@"
 exit
